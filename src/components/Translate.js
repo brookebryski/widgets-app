5540c7dedf3b02d51375b75bf8942e68c6b2ab61
github username <github email address>
2021-04-26 11:58:59 -0500

    finished convert component

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,4 +1,3 @@
-//AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
 import Convert from './Convert';
@@ -15,7 +14,11 @@ const options = [
 {
     label:'Hindi',
     value: 'hi'
- }
+ },
+ {
+   label: 'Dutch',
+   value: 'nl',
+ },
 ];
 
 const Translate = () => {
@@ -37,7 +40,7 @@ const Translate = () => {
            />
            <hr />
            <h3 className="ui header">Output</h3>
-           <Convert tet={text} language={language}/>
+           <Convert text={text} language={language}/>
         </div>
     );
 };
